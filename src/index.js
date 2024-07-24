a25ec8e32fa32db8f1d07e2ce336ae64b87601cf
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-07-24 01:52:45 -0300

    add modules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('hello')
+import prompt from "prompt"
+import chalk from "chalk";
+
+import mainPrompt from "./prompts/prompt-main.js"
+
+async function main(){ 
+    prompt.get(mainPrompt, async (err, choose) => {
+        if (choose.select == 1) console.log("escolheu o qrCODE");
+        if (choose.select == 2) console.log("escolheu o password");
+    });
+
+    prompt.start();
+}
+
+main()
