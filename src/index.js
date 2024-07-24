b02af459dc130c37528eddb112b4e8a2125d8df7
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-07-24 13:59:49 -0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import prompt from "prompt"
 
-import mainPrompt from "./prompts/prompt-main.js"
+import mainPrompt from "./prompts-schema/schema-main.js"
 import createQRCode from "./services/qr-code/create.js";
 import createPassword from "./services/password/create.js";
 
--- src/services/qr-code/create.js
+++ src/services/qr-code/create.js
@@ -1,5 +1,5 @@
 import prompt from "prompt";
-import promptQRCode from "../../prompts/prompt-qrcode.js";
+import promptQRCode from "../../prompts-schema/schema-qrcode.js";
 import handle from "../../services/qr-code/handle.js"
 
 async function createQRCode() {
